0f80251761a27c5904908c4fdee386bac13ea6a0
Vlad <vald.nazarov95@yandex.ru>
2025-01-04 19:37:15 +0300

    1-start: сделал компонент карточки и лайков

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,9 @@
-import { Htag, Ptag, Tag } from "@/components";
-import styles from "./page.module.css";
-import { Button } from "@/components";
+import { PostCard } from "@/components";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <Button appearance="primary" arrow="right">
-        Сохранить
-      </Button>
-      <Button appearance="ghost" arrow="down">
-        Очистить
-      </Button>
-      <Htag tag="h1">Заголовок</Htag>
-      <Ptag size="m">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
-        eaque sit aut, veritatis nesciunt quam voluptas repudiandae, illum,
-        suscipit earum hic optio. Aliquam optio sequi soluta reprehenderit
-        laudantium, expedita necessitatibus.
-      </Ptag>
-      <Tag color="red" size="m">
-        Test
-      </Tag>
+    <div className="root">
+      <PostCard />
     </div>
   );
 }
---
+++ components/PostCard/PostCard.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import styles from "./PostCard.module.css";
+import { Ptag } from "../Ptag/Ptag";
+import PointSVG from "./point.svg";
+import ArrowSVG from "./arrow.svg";
+import { Likes } from "../Likes/Likes";
+
+export const PostCard = () => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.image}>
+        <Image
+          className={styles.photo}
+          src="/post-card.png"
+          alt="Picture of the author"
+          fill
+        />
+      </div>
+
+      <div className={styles.description}>
+        <div className={styles.description__header}>
+          <div className={styles.date}>
+            <span>Front-end</span>
+            <PointSVG />
+            <span>1 месяц назад</span>
+          </div>
+          <Likes count={4} />
+        </div>
+
+        <h3 className={styles.description__title}>Как работать с CSS Grid</h3>
+
+        <Ptag size="s" className={styles.description__info}>
+          Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему
+          сеток в CSS. Гриды подойдут и для верстки основных областей страницы..
+        </Ptag>
+      </div>
+
+      <div className={styles.footer}>
+        <span>3 минуты</span>
+        <button>
+          Читать
+          <ArrowSVG />
+        </button>
+      </div>
+    </div>
+  );
+};
